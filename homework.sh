eb92c79d8440ac42771eff3087d44a86fe348ec4
root <root@UbuntuLTS.myguest.virtualbox.org>
2022-11-08 09:30:44 +0200

    Homework from module 2

---
+++ homework.sh
@@ -0,0 +1,10 @@
+ #! /bin/bash/
+
+user_name=$(whoami)
+echo "You are loged as $user_name"
+sleep 2
+dir_name=$(pwd)
+echo "you are in the $dir_name directory"
+sleep 2
+echo "Today is $(date)"
+sleep 2
